cc376311b917425986dc3c31d9ffeeb7698ae42b
rbeuthin <rbeuthin@users.noreply.github.com>
2024-09-13 17:26:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,8 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
+resource "aws_s3_bucket" "rbeubucket01" {
+    bucket = "rbeubucket01"
 
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
-}
-
-resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = "t3.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
+    tags = {
+        Name = "rbeubucket"
+        Environment = "DV"
+    }
 }
